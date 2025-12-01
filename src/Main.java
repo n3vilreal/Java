b7327ff7a1dcd2ae5003baf806933f1e26fb14ae
n3vilreal <nebil.241423@ncit.edu.np>
2025-12-01 19:19:08 +0545

    Java first

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Student std = new Student("Hari Bahadur", 19);
+        std.getName();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int roll;
+    String name;
+    public Student(String pname, int proll){
+        roll = proll;
+        name = pname;
+    }
+    public  void getName(){
+        System.out.println(name);
+    }
+}
