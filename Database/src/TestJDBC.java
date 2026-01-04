35227aa8a43176e66c51fcd89e52471b1688202f
n3vilreal <nebil.241423@ncit.edu.np>
2026-01-04 16:18:36 +0545

    connecting mysql database, try

---
+++ Database/src/TestJDBC.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        try {
+            // 🔴 ADD THIS LINE
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/testdb?useSSL=false&allowPublicKeyRetrieval=true";
+            String user = "javauser";
+            String password = "javapass";
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to MySQL successfully!");
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
